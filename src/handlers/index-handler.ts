23d9db1681b730d16b6deb85fe4047042b5698c9
Shadman Kolahzary <shadman.ko@gmail.com>
2021-03-25 15:33:24 +0430

    style: lint fix

--- src/handlers/index-handler.ts
+++ src/handlers/index-handler.ts
@@ -6,15 +6,16 @@ interface VM {
     emoji: string;
 }
 
-const tpl = (vm: VM) => `<!DOCTYPE html>
+const tpl = (vm: VM) => 
+`<!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="utf-8">
-        <title> Shart Info </title>
-    </head>
-    <body>
-        <h1> You're connecting from ${vm.country} ${vm.emoji} country to our ${vm.colo} datacenter </h1>
-    </body>
+<head>
+    <meta charset="utf-8">
+    <title> Shart Info </title>
+</head>
+<body>
+    <h1> You're connecting from ${vm.country} ${vm.emoji} country to our ${vm.colo} datacenter </h1>
+</body>
 </html>
 `
 
