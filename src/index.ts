d2cc83350d2f004e5b78f9b17809e592ec3f6030
Shadman Kolahzary <shadman.ko@gmail.com>
2021-03-27 09:38:09 +0430

    fix: handle exceptions

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,18 @@
+import { DEBUG } from './environment'
 import { handleRequest } from './handler'
 
+
 addEventListener('fetch', (event) => {
-  event.respondWith(handleRequest(event.request))
+  try {
+    event.respondWith(handleRequest(event.request))
+  } catch (e) {
+    if (DEBUG) {
+      return event.respondWith(
+        new Response(e.message || e.toString(), {
+          status: 500,
+        }),
+      )
+    }
+    event.respondWith(new Response('Internal Error', { status: 500 }))
+  }
 })
